927bd24ef33a8efc6dfa543f73bca9137f33de25
Koki Sakai <likuor0528@gmail.com>
2022-09-06 13:23:15 -0700

    Add Backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+type type = {
+  users: string[];
+};
 
 function App() {
+  const [backendData, setBackendData] = useState<type>();
+
+  useEffect(() => {
+    fetch('/api')
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {backendData === undefined ? (
+        <p>Loading...</p>
+      ) : (
+        backendData.users.map((user, index) => <p key={index}>{user}</p>)
+      )}
+    </>
   );
 }
 
