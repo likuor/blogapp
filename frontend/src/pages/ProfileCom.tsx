817f6e55ee11a15560b12e68a873aa646195dc9e
Koki Sakai <likuor0528@gmail.com>
2022-09-20 23:02:03 -0700

    Add profile form

--- frontend/src/pages/ProfileCom.tsx
+++ frontend/src/pages/ProfileCom.tsx
@@ -1,10 +1,10 @@
-import React, { FC, useState, useEffect, useContext } from 'react';
+import React, { FC, useRef, useContext } from 'react';
 import Layout from '../Layout/Layout';
-import axios from 'axios';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import { AuthContext } from '../state/AuthContext';
+import Form from 'react-bootstrap/Form';
+import ButtonCom from '../components/ButtonCom';
+import axios from 'axios';
 
 interface LoginState {
   user: {
@@ -17,38 +17,77 @@ interface LoginState {
     updatedAt: string;
     username: string;
     __v?: number;
-    _id: string | null;
+    _id: string | any;
   } | null;
 }
 
 const ProfileCom: FC = () => {
+  const refUserName = useRef<HTMLInputElement>(null);
+  const refDescription = useRef<HTMLInputElement>(null);
+  const refEmail = useRef<HTMLInputElement>(null);
+  const refProfilePiture = useRef<HTMLInputElement>(null);
+
   const { user }: LoginState = useContext(AuthContext);
   console.log(user);
 
-  // const [user, setUser] = useState(undefined);
+  const handleSubmit = async () => {
+    const updatedUser = {
+      userId: user?._id,
+      username: refUserName.current?.value,
+      description: refDescription.current?.value,
+      email: refEmail.current?.value,
+      profilePicture: refProfilePiture.current?.value,
+    };
 
-  // useEffect(() => {
-  //   const fetchArticles = async () => {
-  //     const response = await axios.get('/users?username=koki');
-  //     setUser(response.data);
-  //   };
-  //   fetchArticles();
-  // }, []);
+    console.log(updatedUser);
+
+    await axios
+      .put(`/api/users/${user?._id}`, updatedUser)
+      .catch((error) => console.log('error', error));
+  };
 
   return (
     <Layout>
       <Container fluid>
-        <Row>
-          <Col>Username: {user?.username}</Col>
-        </Row>
-        <Row>
-          <Col>Email: {user?.email}</Col>
-        </Row>
-        <Row>
-          <Col>
-            <img src={user?.profilePicture} alt={`${user?.username} pic`} />
-          </Col>
-        </Row>
+        <Form method='put' action={`/api/users/${user?._id}`}>
+          <Form.Group className='mb-3' controlId='formGroupText'>
+            <Form.Label>Username: {user?.username}</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='User name'
+              ref={refUserName}
+            />
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='formGroupText'>
+            <Form.Label>Description: {user?.description}</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Description'
+              ref={refDescription}
+            />
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='formGroupText'>
+            <Form.Label>Email: {user?.email}</Form.Label>
+            <Form.Control type='text' placeholder='Email' ref={refEmail} />
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='formGroupText'>
+            <Form.Label>
+              Profile piture:
+              <img src={user?.profilePicture} alt={`${user?.username} pic`} />
+            </Form.Label>
+            <Form.Control
+              type='textarea'
+              placeholder='Profile piture'
+              ref={refProfilePiture}
+            />
+          </Form.Group>
+        </Form>
+        <ButtonCom
+          text='Update'
+          color='primary'
+          type='submit'
+          onClick={handleSubmit}
+        />
       </Container>
     </Layout>
   );
